db6aff9f5542215fc51415ec04c5248ff8689bfd
Giggs3244 <bryan.bedoy@gmail.com>
2022-06-17 17:37:57 -0500

    fix: Se utiliza render de react v17

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { render } from 'react-dom'
+import AppProvider from './app/main/view/AppProvider';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootElement = document.getElementById("root");
+render(<AppProvider />, rootElement);
