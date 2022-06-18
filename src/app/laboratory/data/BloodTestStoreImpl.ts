6e9fb0e6ca9a23d42836f0770c393e15f5ff35df
Giggs3244 <bryan.bedoy@gmail.com>
2022-06-18 12:42:33 -0500

    feat: a la implementacion del store se agrega funcion para agregar un blood test

--- src/app/laboratory/data/BloodTestStoreImpl.ts
+++ src/app/laboratory/data/BloodTestStoreImpl.ts
@@ -1,15 +1,27 @@
-import { useSelector } from "react-redux";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { AppRootState } from "../../main/data/appStore";
+import { BloodTest } from "../domain/BloodTestEntity";
 import { BloodTestStore } from "../domain/BloodTestStore";
-import { BloodTestState } from "./BloodTestSlice";
+import { add, BloodTestState } from "./BloodTestSlice";
 
 const bloodTestSelector = (state: AppRootState) => state.bloodTest;
 
 const useBloodTestStoreImplementation = (): BloodTestStore => {
-  const { bloodTests } = useSelector(bloodTestSelector);
+  const { bloodTests } = useSelector<AppRootState, BloodTestState>(bloodTestSelector);
+
+  const dispatch = useDispatch();
+
+  const addBloodTest = React.useCallback(
+    (bloodTest: BloodTest) => {
+      dispatch(add(bloodTest));
+    },
+    [dispatch]
+  );
 
   return {
-    bloodTests
+    bloodTests,
+    addBloodTest,
   };
 };
 
