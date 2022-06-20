ff23785380f4e71a399bfa3306aaf26764c805dd
Giggs3244 <bryan.bedoy@gmail.com>
2022-06-19 19:47:49 -0500

    feat: se crea servicio para consumir api de dogs, se crea entidad modelo, se crea slice de dogs y se agrega en el store

--- src/app/main/data/appStore.ts
+++ src/app/main/data/appStore.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import DogsSlice from "../../dogs/data/DogsSlice";
 import BloodTestSlice from "../../laboratory/data/BloodTestSlice";
 
+
 export const store = configureStore({
-  reducer: { bloodTest: BloodTestSlice }
+  reducer: { bloodTest: BloodTestSlice, dogs: DogsSlice },
 });
 
 export type AppDispatch = typeof store.dispatch;
